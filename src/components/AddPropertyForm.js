65420776088b0e90c2a3747707b970feed46829a
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-07 15:48:15 +0100

    ajout d'un formulaire simple

---
+++ src/components/AddPropertyForm.js
@@ -0,0 +1,99 @@
+// src/components/AddPropertyForm.js
+import React, { useState } from 'react';
+
+function AddPropertyForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    price: '',
+  });
+  const [errors, setErrors] = useState({});
+  
+  const validateForm = () => {
+    let formIsValid = true;
+    let errors = {};
+
+    if (!formData.title) {
+      formIsValid = false;
+      errors['title'] = 'Le titre est requis.';
+    }
+
+    if (!formData.description) {
+      formIsValid = false;
+      errors['description'] = 'La description est requise.';
+    }
+
+    if (!formData.price) {
+      formIsValid = false;
+      errors['price'] = 'Le prix est requis.';
+    } else if (isNaN(formData.price)) {
+      formIsValid = false;
+      errors['price'] = 'Le prix doit être un nombre.';
+    }
+
+    setErrors(errors);
+    return formIsValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      console.log('Envoi des données du formulaire:', formData);
+      // Ici, tu pourrais envoyer les données à un serveur
+      // Simulons un délai de réponse avec setTimeout
+      setTimeout(() => {
+        alert('Propriété ajoutée avec succès!');
+        // Réinitialiser le formulaire ici si nécessaire
+        setFormData({
+          title: '',
+          description: '',
+          price: '',
+        });
+      }, 1000);
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Titre:</label>
+        <input
+          type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        {errors.title && <p>{errors.title}</p>}
+      </div>
+      <div>
+        <label>Description:</label>
+        <textarea
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+        />
+        {errors.description && <p>{errors.description}</p>}
+      </div>
+      <div>
+        <label>Prix:</label>
+        <input
+          type="text"
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+        />
+        {errors.price && <p>{errors.price}</p>}
+      </div>
+      <button type="submit">Ajouter la propriété</button>
+    </form>
+  );
+}
+
+export default AddPropertyForm;
--- src/container/Contact.js
+++ src/container/Contact.js
@@ -1,10 +1,14 @@
 import React from "react";
+import AddPropertyForm from '../components/AddPropertyForm';
 
 function Contact() {
   return (
     <div>
       <h1>Contact</h1>
       <p>Bienvenue sur la page Contact de notre application!</p>
+      <hr/>
+      <h2>Ajouter une nouvelle propriété</h2>
+      <AddPropertyForm />
     </div>
   );
 }

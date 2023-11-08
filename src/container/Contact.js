87375a2d884b98334008c514c85efa4d35dcb3c1
Yohann Loïc Ravino <atticot.yohann@gmail.com>
2023-11-08 09:07:14 +0100

    setup Firebase and Crud operation

--- src/container/Contact.js
+++ src/container/Contact.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ProductForm from '../components/ProductForm';
 
 function Contact() {
   return (
     <div>
       <h1>Contact</h1>
       <p>Bienvenue sur la page Contact de notre application!</p>
+      <ProductForm/>
     </div>
   );
 }
